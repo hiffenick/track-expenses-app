503e249931c62a18cb3eb94fd75ff546aeb0c9fc
Nikhil <kamthenikhil654@gmail.com>
2026-06-06 13:27:08 +0530

    refact:smtp to resend due to persistent smtp issue

--- src/login.py
+++ src/login.py
@@ -1,23 +1,24 @@
 import random
-from flask import Blueprint,render_template,request,redirect,url_for,session
-from flask_mail import Message
-from src.extensions import loginmanager,mail
-from flask_login import login_user,current_user
-from werkzeug.security import check_password_hash
-from src.extensions import bcrypt,limiter
+import resend
+import os
+from flask import Blueprint, render_template, request, redirect, url_for, session
+from src.extensions import loginmanager
+from flask_login import login_user, current_user
+from src.extensions import bcrypt, limiter
 from src.models.user import User
 from src.wtform import LoginForm
 
-login_route = Blueprint('login',__name__)
+resend.api_key = os.getenv("RESEND_API_KEY")
+
+login_route = Blueprint('login', __name__)
 
 @loginmanager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
-@login_route.route('/login',methods=['GET','POST'])
-@limiter.limit("10 per minute")        # max 10 attempts per minute per IP
-@limiter.limit("20 per hour")          # max 20 attempts per hour per IP
+@login_route.route('/login', methods=['GET', 'POST'])
+@limiter.limit("10 per minute")
+@limiter.limit("20 per hour")
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('dashboard.dashboard'))
@@ -25,36 +26,20 @@ def login():
     form = LoginForm()
 
     if request.method == 'POST' and form.validate_on_submit():
-
-        user = User.query.filter_by(user_mail = form.email.data).first()
-        if user and bcrypt.check_password_hash(user.user_pass , form.password.data):
-            print('login sucess')
-
-            #Genertae Otp for Authentification
-            otp = str(random.randint(100000,999999))
+        user = User.query.filter_by(user_mail=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.user_pass, form.password.data):
+            otp = str(random.randint(100000, 999999))
             session['user_id'] = user.user_id
             session['otp'] = otp
             session['otp_verified'] = False
 
-            #Send Otp Main
-            msg = Message(
-                subject="Your Expense Tracker OTP",
-                recipients=[user.user_mail]
-            )
+            resend.Emails.send({
+                "from": "onboarding@resend.dev",
+                "to": user.user_mail,
+                "subject": "Your Expense Tracker OTP",
+                "text": f"Hello {user.user_name},\n\nYour OTP is: {otp}\n\nDo not share this OTP with anyone.\n\n- Expense Tracker"
+            })
 
-            msg.body = f"""
-            Hello {user.user_name},
-
-            Your OTP is: {otp}
-
-            Do not share this OTP with anyone.
-
-            - Expense Tracker
-            """
-
-            mail.send(msg)
-
-            # print("OTP SENT:", otp)
             return redirect(url_for('verify.verify'))
 
-    return render_template('login.html',form = form)
+    return render_template('login.html', form=form)
