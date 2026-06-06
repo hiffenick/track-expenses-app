9dde0b41829f6579dbeaca3bd46997c1ff3978f2
Nikhil <kamthenikhil654@gmail.com>
2026-06-06 12:50:40 +0530

    MAIL_USE_SSL added

--- src/config.py
+++ src/config.py
@@ -23,6 +23,7 @@ class Config:
     MAIL_USERNAME       = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD       = os.getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+    MAIL_USE_SSL = True
 
     # ── Database ────────────────────────────────────────────────
     SQLALCHEMY_DATABASE_URI = os.getenv(
