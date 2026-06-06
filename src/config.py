1179a1f567756e7387a5d61b1575ebbd8f5e5296
Nikhil <kamthenikhil654@gmail.com>
2026-06-06 12:54:07 +0530

    session type changed

--- src/config.py
+++ src/config.py
@@ -47,7 +47,7 @@ class Config:
 
     # Strict SameSite prevents CSRF via cross-site requests
     SESSION_COOKIE_SAMESITE = "Lax"
-    SESSION_TYPE = 'filesystem'
+    SESSION_TYPE = 'sqlalchemy'
     SESSION_FILE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'flask_sessions')
     SESSION_USE_SIGNER = True
 
